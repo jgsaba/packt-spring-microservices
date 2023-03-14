9b0ffe1147edce7a6e33b963dfb203c818e01252
Jorge_Saba <Jorge_Saba@epam.com>
2023-03-14 11:17:35 -0300

    adding persistence layer and tests

--- microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -1,14 +1,23 @@
 package se.magnus.microservices.core.product;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ProductServiceApplication.class, args);
+    ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+
+    log.info(String.format("Connected to MongoDb: %s:%s",
+            ctx.getEnvironment().getProperty("spring.data.mongodb.host"),
+            ctx.getEnvironment().getProperty("spring.data.mongodb.port")));
   }
+
+
 }
---
+++ microservices/product-service/src/main/java/se/magnus/microservices/core/product/persistence/ProductRepository.java
@@ -0,0 +1,11 @@
+package se.magnus.microservices.core.product.persistence;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String> {
+
+    Optional<ProductEntity> findByProductId(int productId);
+
+}
